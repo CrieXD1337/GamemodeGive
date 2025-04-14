4cb84a14f76a10d68d3db2bdb6525b6117295876
Ivan <sunblog0bg@gmail.com>
2025-04-15 00:49:32 +0300

    Added whitelist

--- src/main/java/ru/rexlite/Loader.java
+++ src/main/java/ru/rexlite/Loader.java
@@ -22,26 +22,34 @@ public class Loader extends PluginBase implements Listener {
         if (cmd.getName().toLowerCase().equals("gamemodegive")) {
             if (args.length < 2) {
                 sender.sendMessage(TextFormat.colorize(cfg.getString("use")));
+                return false;
+            }
+
+            String playerName = args[0];
+            String type = args[1];
+            var targetPlayer = this.getServer().getPlayer(playerName);
+
+            if (targetPlayer == null) {
+                sender.sendMessage(TextFormat.colorize(cfg.getString("not-found").replace("{player}", playerName)));
+                return false;
+            }
+
+            if (targetPlayer.hasPermission("gamemode.give.ignored")) {
+                sender.sendMessage(TextFormat.colorize(cfg.getString("player-in-whitelist").replace("{player}", targetPlayer.getName())));
+                return false;
+            }
+
+            if (type.equals("true")) {
+                targetPlayer.setGamemode(1); // Creative
+                sender.sendMessage(TextFormat.colorize(cfg.getString("true-gamemode").replace("{player}", targetPlayer.getName())));
+                if (cfg.getBoolean("is-use-messages-for-players")) {
+                    targetPlayer.sendMessage(TextFormat.colorize(cfg.getString("message-true")));
+                }
             } else {
-                String player = args[0];
-                String type = args[1];
-                if (this.getServer().getPlayer(player) != null) {
-                    player = this.getServer().getPlayer(player).getName();
-                    if (type.equals("true")) {
-                        this.getServer().getPlayer(player).setGamemode(1);
-                        sender.sendMessage(TextFormat.colorize(cfg.getString("true-gamemode").replace("{player}", player)));
-                        if (cfg.getBoolean("is-use-messages-for-players")) {
-                            this.getServer().getPlayer(player).sendMessage(TextFormat.colorize(cfg.getString("message-true")));
-                        }
-                    } else {
-                        this.getServer().getPlayer(player).setGamemode(0);
-                        sender.sendMessage(TextFormat.colorize(cfg.getString("false-gamemode").replace("{player}", player)));
-                        if (cfg.getBoolean("is-use-messages-for-players")) {
-                            this.getServer().getPlayer(player).sendMessage(TextFormat.colorize(cfg.getString("message-false")));
-                        }
-                    }
-                } else {
-                    sender.sendMessage(TextFormat.colorize(cfg.getString("no-found").replace("{player}", player)));
+                targetPlayer.setGamemode(0); // Survival
+                sender.sendMessage(TextFormat.colorize(cfg.getString("false-gamemode").replace("{player}", targetPlayer.getName())));
+                if (cfg.getBoolean("is-use-messages-for-players")) {
+                    targetPlayer.sendMessage(TextFormat.colorize(cfg.getString("message-false")));
                 }
             }
         }
